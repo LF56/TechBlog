0e831064bf55ce3b00d84449b1ad0b97f67885f8
LF56 <leah.fox7@gmail.com>
2022-03-24 08:26:26 -0400

    created user seeds

---
+++ seeds/user-seeds.js
@@ -0,0 +1,58 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userInfo = [
+    {
+        username: 'leahf21',
+        email: 'leahf21@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'racheljohn25',
+        email: 'rjohnson@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'walkerj75',
+        email: 'walkerj75@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'bobbyj32',
+        email: 'jeanbob@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'loladavid4',
+        email: 'lodavid@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'kahold45',
+        email: 'katehold@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'holkai33',
+        email: 'hollyk3@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'derrik295',
+        email: 'derrikmc2@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'car9213',
+        email: 'carlossmith@email.com',
+        password: 'password000'
+    },
+    {
+        username: 'luis45',
+        email: 'luisday4@email.com',
+        password: 'password000'
+    },
+];
+const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+
+module.exports = seedUsers;
